64d87390770ea9f462d43e7cd3f260989ab7b9dd
Mattia Mari <mattiamari96@gmail.com>
2019-11-20 16:39:21 +0100

    Add main file for arduino

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <scheduler.h>
+#include <scanner.h>
+#include <scanstatus.h>
+
+smartradar::Scheduler scheduler = smartradar::Scheduler();
+smartradar::ScanStatus scanStatus = smartradar::ScanStatus();
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
